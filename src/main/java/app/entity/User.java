82ab1967dac551839f5fb4a92cc9ac2554df4155
Junior <Silva>
2018-02-28 17:48:02 +0000

    teste com email informado, no problema de autoria de

--- src/main/java/app/entity/User.java
+++ src/main/java/app/entity/User.java
@@ -11,7 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 /**
- * Classe que representa a tabela Edson Silva! teste Mateus da Bahia 2 alterado
+ * Classe que representa a tabela Edson Silva! teste Mateus da Bahia 2 alterado 2
  * @generated
  */
 @Entity
