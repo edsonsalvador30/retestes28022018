b17c374407e985b377ec14192d13503a1992b9a8
juninho2 <edson.fonseca@techne.com.br>
2018-02-28 17:18:32 +0000

    Juninho2222

--- src/main/java/app/entity/User.java
+++ src/main/java/app/entity/User.java
@@ -11,7 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 /**
- * Classe que representa a tabela Edson Silva! teste
+ * Classe que representa a tabela Edson Silva! teste Junionho2222
  * @generated
  */
 @Entity
